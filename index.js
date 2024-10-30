2d162db1409cfea319492449f7f7692e45b08ff6
carolinekroeger <123000427+carolinekroeger@users.noreply.github.com>
2024-10-30 10:27:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const switchList = ()=> {
+    const scaryIsChecked = document.getElementById('Scary').checked;
+    const funIsChecked = document.getElementById('Fun').checked;
+}
+
+if (scaryIsChecked) {
+    
+}
